3cc1b416d07bd6addb86bdb9449a068cf10a5cd5
Lara Schulze <laraschulze@MacBook-Air-von-Lara.fritz.box>
2025-12-01 00:44:04 +0100

    h3

--- h3/H3_main.java
+++ h3/H3_main.java
@@ -1,40 +1,42 @@
 package h3;
 
 public class H3_main {
-    public static boolean compareArraysVal (int[] a, int[] b) {
+public static boolean compareArraysVal (int[] a, int[] b) {
         if (a.length != b.length) return false;
 
         for (int i = 0; i < a.length; i++) {
-            int countA = 0;
-            int countB = 0;
+            int HäufigkeitA = 0;
+            int HäufigkeitB = 0;    
 
             for (int j = 0; j < a.length; j++) {
                 if (a[j] == a[i]) { 
-                    countA++;
+                    HäufigkeitA++;
             }
         }
         for (int j = 0; j < b.length; j++) {
             if (b[j] == a[i]) {
-                countB++;
+                HäufigkeitB++;
             }
+
         }
-        if (countA != countB) return false;
 
-    }          
+        if (HäufigkeitA != HäufigkeitB) return false;
     
-    return true;
-
+    }
+    return true;    
 }
 
 
-//meine eigenen Testfälle
+
+// meine eigenen Testfälle
 public static void main(String[] args) {
         int[] a = {1, 2, 3};
         int[] b = {1, 2, 4};
         int[] c = {1, 3, 2};
-        int[] d = {1, 2};
-        int[] e = {1, 2, 3};
-        int[] f = {1, 1, 2};
+        int[] d = {1, 2};       
+         int[] e = {1, 2, 3};
+        int[] f = {1, 1, 2};                        
+
 
         System.out.println(compareArraysVal(a, c));
         System.out.println(compareArraysVal(a, e));
@@ -46,3 +48,4 @@ public static void main(String[] args) {
 }
 
 }
+
